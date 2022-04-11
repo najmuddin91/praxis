9e0baea9e46567f858287548b2986e8de17a0f36
najmuddin91 <kendil.lawas@gmail.com>
2022-04-11 23:27:35 +0700

    data baru

---
+++ practice/main.js
@@ -0,0 +1,3 @@
+JavaScript adalah bahasa pemrograman yang menambahkan interaktivitas ke situs web Anda.
+JavaScript serbaguna dan ramah-pemula. Dengan lebih banyak pengalaman, Kita akan dapat membuat game, animasi grafis 2D dan 3D, aplikasi berbasis database yang komprehensif, dan banyak lagi!
+
---
+++ practice/practice.js
@@ -0,0 +1,26 @@
+// // let a = 10
+// // let b = 20
+// // let c = 30
+
+// // if(a + b === c)
+// // console.log(sarjan)
+// // else if (a + c === b)
+// // console.log(balik sd)
+
+// // var a = 10;
+// // var b = 30;
+// // var c = a + b;
+// // console.log(c);
+
+// // var a = 3;
+// // var b = 7;
+// // var c = a + b;
+// // console.log(c);
+
+// let a = 5
+// let b = 10
+// let c = 50
+// if ( a * b === c )
+// console.log("benar")
+// else
+// console.log("salah")
