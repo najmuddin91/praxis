5afd7bda04803a1679b3cfc44497ede51e416a60
najmuddin91 <kendil.lawas@gmail.com>
2022-06-24 22:34:17 +0700

    titik

--- practice/project-Vinance/src/Components/Register.jsx
+++ practice/project-Vinance/src/Components/Register.jsx
@@ -11,7 +11,7 @@ function Register() {
       </Form.Item>
 
       <Form.Item name="email" label="Email">
-        <Input placeholder="Masukan Email Maszzeeh"/>
+        <Input placeholder="Type your email"/>
       </Form.Item>
 
       <Form.Item name="password" label="Password">
